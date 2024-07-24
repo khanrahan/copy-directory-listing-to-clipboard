f5a506d95f72674366dfc5da49ef8e65b2633ee0
Kieran Hanrahan <kieran@knhn.me>
2024-07-23 20:06:54 -0400

    Make inline quotes consistent

--- copy_dir_listing_to_clipboard.py
+++ copy_dir_listing_to_clipboard.py
@@ -31,17 +31,17 @@ To Install:
 
 from __future__ import print_function
 
-__title__ =  "Copy Directory List to Clipboard"
-__version_info__ = (0, 1, 0)
-__version__ = ".".join([str(num) for num in __version_info__])
-__title_version__ = "{} v{}".format(__title__, __version__)
+__title__ =  'Copy Directory List to Clipboard'
+__version_info__ = (0, 1, 0, 'dev')
+__version__ = '.'.join([str(num) for num in __version_info__])
+__title_version__ = '{} v{}'.format(__title__, __version__)
 
-MESSAGE_PREFIX = "[PYTHON HOOK]"
+MESSAGE_PREFIX = '[PYTHON HOOK]'
 
 def message(string):
     """Print message to shell window and append global MESSAGE_PREFIX."""
 
-    print(" ".join([MESSAGE_PREFIX, string]))
+    print(' '.join([MESSAGE_PREFIX, string]))
 
 
 def copy_to_clipboard(text):
@@ -58,7 +58,7 @@ def dir_listing(path):
 
     import os
 
-    dir_list = [f for f in os.walk(path).next()[2] if not f[0] == "."]
+    dir_list = [f for f in os.walk(path).next()[2] if not f[0] == '.']
 
     return dir_list
 
@@ -69,26 +69,26 @@ def gather_listings(selection, sort):
     import os
 
     message(__title_version__)
-    message("Script called from {}".format(__file__))
+    message('Script called from {}'.format(__file__))
 
-    results = ""
+    results = ''
 
     for folder in selection:
         if not len(results) == 0:
-            results += "\n"  # separate from previous listing by 1 line
+            results += '\n'  # separate from previous listing by 1 line
 
         results += folder.path
-        results += "\n"
+        results += '\n'
 
         listing = dir_listing(folder.path)
-        message("Added listing of {}".format(folder.path))
+        message('Added listing of {}'.format(folder.path))
 
-        if sort == "name":
+        if sort == 'name':
             listing.sort()
-        if sort == "date":
+        if sort == 'date':
             listing.sort(key=lambda f: os.path.getmtime(os.path.join(folder.path, f)))
 
-        results += "\n".join(listing)
+        results += '\n'.join(listing)
 
     return results
 
@@ -96,37 +96,37 @@ def gather_listings(selection, sort):
 def dir_listing_to_clipboard(selection):
     """Copy directory listing sorted by name to system clipboard."""
 
-    results = gather_listings(selection, sort = "name")
+    results = gather_listings(selection, sort = 'name')
     copy_to_clipboard(results)
-    message("Copied to clipboard!")
+    message('Copied to clipboard!')
 
 
 def dir_listing_by_date_to_clipboard(selection):
     """Copy directory listing sorted by date to the system clipboard."""
 
-    results = gather_listings(selection, sort = "date")
+    results = gather_listings(selection, sort = 'date')
     copy_to_clipboard(results)
-    message("Copied to clipboard!")
+    message('Copied to clipboard!')
 
 
 def scope_folders(selection):
     """Determine if selection is a folder in the Media Hub > Files tab."""
 
     for item in selection:
-        if "FilesFolder" in str(item):
+        if 'FilesFolder' in str(item):
             return True
     return False
 
 
 def get_mediahub_files_custom_ui_actions():
 
-    return [{"name" : "Copy...",
-             "actions" : [{"name": "Directory Listing (by Date) to Clipboard",
-                           "isVisible": scope_folders,
-                           "execute": dir_listing_by_date_to_clipboard,
-                           "minimumVersion": "2020.3.1"},
-                          {"name": "Directory Listing (by Name) to Clipboard",
-                           "isVisible": scope_folders,
-                           "execute": dir_listing_to_clipboard,
-                           "minimumVersion": "2020.3.1"}]
+    return [{'name' : 'Copy...',
+             'actions' : [{'name': 'Directory Listing (by Date) to Clipboard',
+                           'isVisible': scope_folders,
+                           'execute': dir_listing_by_date_to_clipboard,
+                           'minimumVersion': '2020.3.1'},
+                          {'name': 'Directory Listing (by Name) to Clipboard',
+                           'isVisible': scope_folders,
+                           'execute': dir_listing_to_clipboard,
+                           'minimumVersion': '2020.3.1'}]
            }]
